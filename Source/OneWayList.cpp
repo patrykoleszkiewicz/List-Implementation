519006e0197d26a759d46c533577730ccb29489c
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-03-30 21:31:55 +0200

    Implement at() method

--- Source/OneWayList.cpp
+++ Source/OneWayList.cpp
@@ -6,7 +6,7 @@ OneWayList<T>::OneWayList()
 	_first = nullptr;
 }
 
-template<typename T>
+template<class T>
 void OneWayList<T>::pushBack(const T& value)
 {
 	if(_first == nullptr)
@@ -29,5 +29,25 @@ void OneWayList<T>::pushBack(const T& value)
 		end->next->value = value;
 		end->next->next = nullptr;
 		++_size;
+	}
+}
+
+template<class T>
+T& OneWayList<T>::at(std::size_t index)
+{
+	if(index < _size)
+	{
+		Element* current = _first;
+		
+		for(std::size_t ite = 1; ite < index; ++ite)
+		{
+			current = current->next;
+		}
+		
+		return current->value;
+	}
+	else
+	{
+		throw std::out_of_range();
 	}
 }
