6a4c24031c25243a54774a0c9056fba2b2e4c8a2
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-03-30 21:20:00 +0200

    Implement pushBack() method

--- Source/OneWayList.cpp
+++ Source/OneWayList.cpp
@@ -5,3 +5,29 @@ OneWayList<T>::OneWayList()
 {
 	_first = nullptr;
 }
+
+template<typename T>
+void OneWayList<T>::pushBack(const T& value)
+{
+	if(_first == nullptr)
+	{
+		_first = new Element();
+		_first->value = value;
+		_first->next = nullptr;
+		++_size;
+	}
+	else
+	{
+		Element* end = _first;
+		
+		while(end->next != nullptr)
+		{
+			end = end->next;
+		}
+		
+		end->next = new Element();
+		end->next->value = value;
+		end->next->next = nullptr;
+		++_size;
+	}
+}
