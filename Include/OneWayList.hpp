21a0a62119a03f1a3ff362cb2434f2fe27f12939
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-03-30 20:46:50 +0200

    Define members of OneWayList

--- Include/OneWayList.hpp
+++ Include/OneWayList.hpp
@@ -1,9 +1,21 @@
 #pragma once
+#include <ostream>
 
+template <class T>
 class OneWayList
 {
 public:
+	void pushBack(const T& value);
+	T& at(std::size_t index);
+	std::size_t size();
+	void print(std::ostream& str);
 	
 private:
-	
+	struct Element
+	{
+		T value;
+		Element* next;
+	};
+	Element* _first;
+	std::size_t _size;
 };
