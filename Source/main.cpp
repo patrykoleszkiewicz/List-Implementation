d547e60064503dc9fbbd9ddc7d65941f86bc7437
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-03-31 12:05:28 +0200

    Add test (sort of)

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,1 +1,14 @@
-int main(){}
+#include "OneWayList.hpp"
+#include <iostream>
+
+int main()
+{
+	//TESTS
+	OneWayList<int> list;
+	list.pushBack(0);
+	list.pushBack(2);
+	list.pushBack(3);
+	std::size_t s = list.size();
+	int i = list.at(1);
+	std::cout << s << " " << i;
+}
