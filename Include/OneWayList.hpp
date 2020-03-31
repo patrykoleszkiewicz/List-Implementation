be94f1a7a6a43bff0f9281ddc41a28f3c36d8db8
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-03-31 12:04:10 +0200

    Move method definitions to OneWayList.hpp

--- Include/OneWayList.hpp
+++ Include/OneWayList.hpp
@@ -5,12 +5,63 @@ template <class T>
 class OneWayList
 {
 public:
-	void pushBack(const T& value);
-	T& at(std::size_t index);
-	std::size_t size() const;
-	void print(std::ostream& str) const;
+	OneWayList()
+	{
+		_first = nullptr;
+		_size = 0;
+	}
 	
-	OneWayList();
+	void pushBack(const T& value)
+	{
+		if(_first == nullptr)
+		{
+			_first = new Element();
+			_first->value = value;
+			_first->next = nullptr;
+			++_size;
+		}
+		else
+		{
+			Element* end = _first;
+			
+			while(end->next != nullptr)
+			{
+				end = end->next;
+			}
+			
+			end->next = new Element();
+			end->next->value = value;
+			end->next->next = nullptr;
+			++_size;
+		}
+	}
+	
+	T& at(std::size_t index)
+	{
+		if(index < _size)
+		{
+			Element* current = _first;
+			
+			for(std::size_t ite = 1; ite < index; ++ite)
+			{
+				current = current->next;
+			}
+			
+			return current->value;
+		}
+		else
+		{
+			//throw std::out_of_range;
+		}
+	}
+	
+	std::size_t size() const
+	{
+		return _size;
+	}
+	
+	//void print(std::ostream& str) const;
+	
 private:
 	struct Element
 	{
